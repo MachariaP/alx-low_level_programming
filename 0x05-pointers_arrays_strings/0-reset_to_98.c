d53f7960c29a2390028af834635246ce774663d1
MachariaP <walburphinehas78@gmail.com>
2023-07-24 01:07:55 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * a function that takes a pointer to an int as parameter
+ *  updates the value it points to to 98
+ *  Return: void
+ */
+
+void reset_to_98(int *x)
+{
+	*x = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - header file for prototypes
+ * void prototypes (viod)
+ * int prototypes(int)
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H */
