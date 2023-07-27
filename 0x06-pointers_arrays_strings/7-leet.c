8cb4661fe4443b07c5cd0319cd341544ada6fcd9
MachariaP <walburphinehas78@gmail.com>
2023-07-27 07:17:26 -0700

    7-leet.c commit revision

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * leet - Encodes a string in 1337
+ *
+ * @str: String to be encoded
  *
  * Return: Always 0.
  */
-int main(void)
+char *leet(char *str)
 {
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+	int a = 0;
+	int b;
+	char s[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
 
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
-    return (0);
+	for (; str[a] != '\0'; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (s[b] == str[a])
+			{
+				str[a] = s1[b];
+			}
+		}
+	}
+	return (str);
 }
