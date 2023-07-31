c7a4458ad9774647f33975149aadcc624197eeea
MachariaP <walburphinehas78@gmail.com>
2023-07-31 14:19:10 -0700

    Correcting my code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	i = 0;
-	while (i < n)
+	while (n)
 	{
-		s[i] = b;
-		i++;
+		s[n-1] = b;
+		n--;
 	}
-
-	return s;
+	return (s);
 }
