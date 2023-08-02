be15fa9071f03e910d80958d108247b78d89308c
MachariaP <walburphinehas78@gmail.com>
2023-08-02 13:41:47 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
 }
