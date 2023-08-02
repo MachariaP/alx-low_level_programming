81c74093d694894fa7e523cbcc56a3230aed98bd
MachariaP <walburphinehas78@gmail.com>
2023-08-02 14:09:58 -0700

    0-puts_recursion.c correction

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,23 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<unistd.h>
-
-/**
- * void prototypes(void)
- * int prototypes(int)
- * char prototypes(char)
- */
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_value(int a, int b);
+int prime(int a, int b);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int length(char *s);
+int check_pelindrome(char *s, int len);
 int wildcmp(char *s1, char *s2);
-int _sqrt(int n, int x);
 
-
-#endif /* MAIN_H */
+#endif
