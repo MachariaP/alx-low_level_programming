da76bba9dfff2c39f7a5d709f0522f90d25fc839
MachariaP <walburphinehas78@gmail.com>
2023-08-04 20:06:49 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen - returns the length of a string
+ *@s: integer entered
+ *Return: void
+ */
+
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; ++i)
+{
+}
+return (i);
+}
