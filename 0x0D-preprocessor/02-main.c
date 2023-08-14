e897f59aa55cf8e9ec3a5dcdbaeffdb69ad4bae0
MachariaP <walburphinehas78@gmail.com>
2023-08-14 03:54:49 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Compiled from: %s\n", __FILE__);
+
+	return 0;
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return 0;
+}
