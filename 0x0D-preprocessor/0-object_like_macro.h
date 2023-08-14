576796cd6bcfc5cee82307bcfc704889ad4187d8
MachariaP <walburphinehas78@gmail.com>
2023-08-14 03:36:59 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef 0-OBJECT_LIKE_MACRO_H
-#define 0-OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* 0-OBJECT_LIKE_MACRO_H */
+#endif /* OBJECT_LIKE_MACRO_H */
