40ea731400a872c43f6738b4064d50b499e87083
MachariaP <walburphinehas78@gmail.com>
2023-08-24 11:15:39 -0700

    Corrected the code of the 1-list_len.c file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * list-len - gets the number of elements in a linked list_t list
- * @h: is a pointer pointing the list
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the list
  *
- * Return: the number of elements in the list
- */
+ *Return: the number of elements
+*/
 
 size_t list_len(const list_t *h)
-{
-	size_t elements = 0;
-
-	while (h)
 	{
-		elements++;
-		h = h->next;
-	}
+		size_t elements = 0;
 
-	return (elements);
-}
+		while (h)
+		{
+			elements++;
+			h = h->next;
+		}
+
+		return (elements);
+	}
