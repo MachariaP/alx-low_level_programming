1d4f55395afb572d4278588a67f0304c0ad96e5e
MachariaP <walburphinehas78@gmail.com>
2023-08-24 10:28:11 -0700

    Created 1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list-len - gets the number of elements in a linked list_t list
+ * @h: is a pointer pointing the list
+ *
+ * Return: the number of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h -> next;
+	}
+
+	return (elements);
+}
