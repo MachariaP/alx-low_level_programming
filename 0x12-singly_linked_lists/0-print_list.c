7ca5ea20db74cf7ad5b4270c3350a5cfa2049684
MachariaP <walburphinehas78@gmail.com>
2023-08-24 06:56:34 -0700

    Created 3 files: .gitignore, lists.h and 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * print_list - Displays all elements in a linked list.
+ *
+ * @h: A pointer to the start of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_list(const list_t *h)
+{
+    const list_t *current = h;
+    size_t node_count = 0;
+
+    /* Traverse the list while the current node is not NULL */
+    while (current != NULL)
+    {
+        /* Check if the string value of the current node is not NULL */
+        if (current->str != NULL)
+        {
+            printf("[%d] %s\n", current->len, current->str);
+        }
+        else
+        {
+            printf("[0] (nil)\n");
+        }
+
+        node_count++;
+        current = current->next;
+    }
+
+    return node_count;
+}
+
