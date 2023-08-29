bbc4aaa607d49abc80481498e37f6a213a514e71
MachariaP <walburphinehas78@gmail.com>
2023-08-29 02:46:56 -0700

    Correction of File: 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,27 +1,20 @@
 #include "lists.h"
-
 /**
-*free_listint2 - frees a linked list
-*@head: pointer to the head of the list
-*
-*Return: void
-*/
-
+ * free_listint2 - frees a list and sets the heaed to Null
+ * @head:  pointer to the begining of the string
+ */
 void free_listint2(listint_t **head)
 {
-listint_t *cursor;
-listint_t **temp = head;
+	listint_t *current, *next;
 
-if (temp != NULL)
-{
-while (*head != NULL)
-{
-cursor = *head;
-free(cursor);
-*head = (*head)->next;
-}
-
-*temp = NULL;
-
-}
+	if (head == NULL)
+		return;
+	current = *head;
+	while (current)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
 }
