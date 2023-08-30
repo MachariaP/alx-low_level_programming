3a76725d4d40ee39e8bcfa4ba241063936970e00
MachariaP <walburphinehas78@gmail.com>
2023-08-30 06:22:30 -0700

    Created File: 102-free_listint_safe.c and added a prototype to File: lists.h

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,55 +1,40 @@
 #include "lists.h"
-
 /**
- * print_listint_safe - function that prints a listint_t linked list.
- * @head: pointer to head of a list.
- *
- * Return: Length of list (INT)
+ *print_listint_safe - prints a listint_t linked list
+ *@head: pointer to the first element
+ *Return: number of nodes on a list
  */
+void print_node(const listint_t *node)
+{
+    printf("[%p] %d\n", (void *)node, node->n);
+}
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow_p = head ,*fast_p = head;
-	size_t ele = 0;
-	int is_loop = 0;
-
-	while (slow_p && fast_p && fast_p->next)
-	{
-		if (!(fast_p->next->next))
-			break;
-		slow_p = slow_p->next;
-		fast_p = fast_p->next->next;
-		if (slow_p == fast_p)
-		{
-			slow_p = slow_p->next;
-			is_loop = 1;
-			break;
-		}
-	}
-
-	if (!is_loop)
-	{
-		while (head)
-		{
-			ele++;
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-		return (ele);
-	}
-
-	while (head)
-	{
-		ele++;
-		if (head == slow_p)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			printf("-> [%p] %d\n", (void *)head, head->next->n);
-			exit(98);
-		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-	}
-	return (0);
+    const listint_t *slow_p = head, *fast_p = head;
+    size_t ele = 0;
+    int is_loop = 0;
+    while (slow_p && fast_p && fast_p->next)
+    {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+        if (slow_p == fast_p)
+        {
+            slow_p = slow_p->next;
+            is_loop = 1;
+            break;
+        }
+    }
+    while (head)
+    {
+        ele++;
+        if (is_loop && head == slow_p)
+        {
+            print_node(head);
+            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+            exit(98);
+        }
+        print_node(head);
+        head = head->next;
+    }
+    return ele;
 }
-
