35de2992e091adad90daa60959a61ab641b5f791
MachariaP <walburphinehas78@gmail.com>
2023-08-31 05:47:45 -0700

    Refactored the code to adhere to the Betty coding style guidelines.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at a given index
- * @n: long integer
- * @index: index of a bit
- * Return: value of bit at index, or -1 if index is out of range
+ *get_bit - returns the value of a bit at a given index
+ *@n: long integer
+ *@index: index of a bit
+ *Return: value of bit at index
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int max_index = sizeof(unsigned long int) * 8 - 1;
+	int bit;
 
-	if (index > max_index)
-	{
-		return (-1)
-	}
-
-	unsigned long int mask = 1UL << index;
-
-	return ((n & mask) !=0);
+	bit = ((n >> index) & 1);
+	if (index > 64)
+		return (-1);
+	return (bit);
 }
+
+
