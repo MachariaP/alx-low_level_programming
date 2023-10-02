a351e6cd2b510f25d57475421cf35607aa0b9e19
MachariaP <walburphinehas78@gmail.com>
2023-10-02 15:46:45 +0300

    modified betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,38 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nrd, nwr;
-    char *buf;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-    /* Check if filename is NULL */
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    /* Open the file in read-only mode */
-    fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-    /* Check if file opening is successful */
-    if (fd == -1)
-        return (0);
+	if (fd == -1)
+		return (0);
 
-    /* Allocate memory for buffer */
-    buf = malloc(sizeof(char) * (letters));
-    if (!buf)
-        return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-    /* Read from the file into the buffer */
-    nrd = read(fd, buf, letters);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-    /* Write the contents of the buffer to STDOUT */
-    nwr = write(STDOUT_FILENO, buf, nrd);
+	close(fd);
 
-    /* Close the file */
-    close(fd);
+	free(buf);
 
-    /* Free the allocated memory */
-    free(buf);
-
-    /* Return the number of bytes written */
-    return (nwr);
+	return (nwr);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,45 +3,36 @@
 /**
  * create_file - creates a file
  * @filename: filename.
- * @text_content: content written in the file.
+ * @text_content: content writed in the file.
  *
- * Return: 1 if it succeeds, -1 if it fails.
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    int nletters;
-    int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-    /* Check if filename is NULL */
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    /* Open the file with write permissions, creating it if it doesn't exist */
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    /* Check if opening the file failed */
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+		return (-1);
 
-    /* If text_content is NULL, set it to an empty string */
-    if (!text_content)
-        text_content = "";
+	if (!text_content)
+		text_content = "";
 
-    /* Calculate the number of letters in text_content */
-    for (nletters = 0; text_content[nletters]; nletters++)
-        ;
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-    /* Write the content to the file */
-    rwr = write(fd, text_content, nletters);
+	rwr = write(fd, text_content, nletters);
 
-    /* Check if writing to the file failed */
-    if (rwr == -1)
-        return (-1);
+	if (rwr == -1)
+		return (-1);
 
-    /* Close the file */
-    close(fd);
+	close(fd);
 
-    /* Return success */
-    return (1);
+	return (1);
 }
