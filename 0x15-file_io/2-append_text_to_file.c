bd54296244b4c7484d33041be7debc302f680a4b
MachariaP <walburphinehas78@gmail.com>
2023-10-02 15:57:22 +0300

    modified betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,43 +5,35 @@
  * @filename: filename.
  * @text_content: added content.
  *
- * Return: 1 if the file exists. -1 if it fails or does not exist.
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    int nletters;
-    int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-    /* Check if filename is NULL */
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    /* Open the file in append mode with write permissions */
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    /* Check if opening the file failed */
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+		return (-1);
 
-    /* If text_content is not NULL, append it to the file */
-    if (text_content)
-    {
-        /* Calculate the number of letters in text_content */
-        for (nletters = 0; text_content[nletters]; nletters++)
-            ;
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-        /* Write the content to the file */
-        rwr = write(fd, text_content, nletters);
+		rwr = write(fd, text_content, nletters);
 
-        /* Check if writing to the file failed */
-        if (rwr == -1)
-            return (-1);
-    }
+		if (rwr == -1)
+			return (-1);
+	}
 
-    /* Close the file */
-    close(fd);
+	close(fd);
 
-    /* Return success */
-    return (1);
+	return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,39 +1,72 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
- *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-int append_text_to_file(const char *filename, char *text_content)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	int fd;
-	int nletters;
-	int rwr;
-
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
+	if (file_from == -1)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
 
-		rwr = write(fd, text_content, nletters);
+/**
+ * main - check the code for ALX students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-		if (rwr == -1)
-			return (-1);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
 	}
 
-	close(fd);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-	return (1);
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
