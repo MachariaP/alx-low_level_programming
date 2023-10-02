8021d7c1b97246d9eb371abb5d3cb667cca22b64
MachariaP <walburphinehas78@gmail.com>
2023-10-02 15:52:53 +0300

    modified betty style of the File: 2-append_text_to_file.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,91 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-void error_file(int file_from, int file_to, char *argv[])
+int append_text_to_file(const char *filename, char *text_content)
 {
-    /* Check if file_from is -1 (error reading from file) */
-    if (file_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	int fd;
+	int nletters;
+	int rwr;
 
-    /* Check if file_to is -1 (error writing to file) */
-    if (file_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
-}
+	if (!filename)
+		return (-1);
 
-/**
- * main - check the code for ALX students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-    int file_from, file_to, err_close;
-    ssize_t nchars, nwr;
-    char buf[1024];
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    /* Check if the number of arguments is not 3 */
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-        exit(97);
-    }
+	if (fd == -1)
+		return (-1);
 
-    /* Open the source file in read-only mode */
-    file_from = open(argv[1], O_RDONLY);
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 
-    /* Open or create the destination file with write permissions */
-    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+		rwr = write(fd, text_content, nletters);
 
-    /* Check if files can be opened */
-    error_file(file_from, file_to, argv);
+		if (rwr == -1)
+			return (-1);
+	}
 
-    /* Read from the source file and write to the destination file */
-    nchars = 1024;
-    while (nchars == 1024)
-    {
-        nchars = read(file_from, buf, 1024);
+	close(fd);
 
-        /* Check for read errors */
-        if (nchars == -1)
-            error_file(-1, 0, argv);
-
-        nwr = write(file_to, buf, nchars);
-
-        /* Check for write errors */
-        if (nwr == -1)
-            error_file(0, -1, argv);
-    }
-
-    /* Close the source file */
-    err_close = close(file_from);
-    if (err_close == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-        exit(100);
-    }
-
-    /* Close the destination file */
-    err_close = close(file_to);
-    if (err_close == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-        exit(100);
-    }
-
-    /* Return success */
-    return (0);
+	return (1);
 }
