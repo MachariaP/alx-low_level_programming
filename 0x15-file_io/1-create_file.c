383a323041d9148c9ce8b68bb2d76158a3c69040
MachariaP <walburphinehas78@gmail.com>
2023-10-30 17:03:46 +0300

    Cleaned betty warnings and errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    int nletters;
-    int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+	return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+	return (-1);
 
-    if (!text_content)
-        text_content = "";
+	if (!text_content)
+	text_content = "";
 
-    for (nletters = 0; text_content[nletters]; nletters++)
-        ;
+	for (nletters = 0; text_content[nletters]; nletters++)
+	;
 
-    rwr = write(fd, text_content, nletters);
+	rwr = write(fd, text_content, nletters);
 
-    if (rwr == -1)
-        return (-1);
+	if (rwr == -1)
+	return (-1);
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,30 +10,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    int nletters;
-    int rwr;
+	int fd;
+	int nletters;
+	int rwr;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+	return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if (fd == -1)
-        return (-1);
+	if (fd == -1)
+	return (-1);
 
-    if (text_content)
-    {
-        for (nletters = 0; text_content[nletters]; nletters++)
-            ;
+	if (text_content)
+	{
+	for (nletters = 0; text_content[nletters]; nletters++)
+	;
 
-        rwr = write(fd, text_content, nletters);
+	rwr = write(fd, text_content, nletters);
 
-        if (rwr == -1)
-            return (-1);
-    }
+	if (rwr == -1)
+	return (-1);
+	}
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
