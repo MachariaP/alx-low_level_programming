9162bcaf930611f866d2343cc0dbebd0a521cd1d
MachariaP <walburphinehas78@gmail.com>
2023-10-30 21:50:32 +0300

    modified File: 0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,10 +7,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-/* Function prototypes */
-
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
+#endif
