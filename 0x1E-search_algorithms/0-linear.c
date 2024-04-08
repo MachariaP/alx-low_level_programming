adfaf5b819ef61c0a935f5686106c0b34755ff79
MachariaP <walburphinehas78@gmail.com>
2024-04-08 12:32:09 +0300

    a function that searches for a value in an array of integers using the Linear search algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers using
+ * the linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: the first index where value is located, or -1 if value is not
+ * present in array or if array is NULL
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,27 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
