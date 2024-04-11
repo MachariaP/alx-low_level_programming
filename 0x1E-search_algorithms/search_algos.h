2c135c52e09de66a5792b471638e9b5f47f3e60a
MachariaP <walburphinehas78@gmail.com>
2024-04-11 17:44:07 +0300

    Betty coding style validation

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -16,14 +16,14 @@
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-
+int interpolation_search(int *array, size_t size, int value);
 #endif /* SEARCH_ALGOS_H */
