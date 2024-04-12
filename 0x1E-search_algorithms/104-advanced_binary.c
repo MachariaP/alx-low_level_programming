a73d61eeb0d4f89fc589870bac31a6368225538c
MachariaP <walburphinehas78@gmail.com>
2024-04-12 20:40:10 +0300

    Implement advanced binary search to find first occurrence of a value

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -41,7 +41,7 @@ int binary_search(int *array, size_t size, int value)
 	size_t mid;
 	int result;
 
-	if (array== NULL || size == 0)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	print_array(array, 0, size);
