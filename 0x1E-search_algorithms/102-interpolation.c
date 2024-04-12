f5eee8ab7ce6d1b8f2a99fbe7e06b0b9879d4b3a
MachariaP <walburphinehas78@gmail.com>
2024-04-12 10:11:14 +0300

    a function that searches for a value in a sorted array of integers using the Interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -2,43 +2,51 @@
 #include "search_algos.h"
 
 /**
- * linear_interpolation - searches for a value in a sorted array of integers
- * using the Linear interpolation search algorithm
+ * interpolation_search - searches for a value in a sorted array of integers
+ *                        using the Interpolation search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
  *
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- *
- * Return: the index where value is located, or -1 on failure
+ * Return: first index where value is located, or -1 if value is not present
+ *         in array or if array is NULL
  */
-
-
 int interpolation_search(int *array, size_t size, int value)
 {
+	/* Initialize low and high for binary search */
 	size_t low = 0, high = size - 1, pos;
-	
+
 	if (array == NULL)
 		return (-1);
 
-	while ((array[high] != array[low]) && (value >= array[low]) && (value <= array[high]))
+	while ((array[high] != array[low]) && (value >= array[low]) &&
+			(value <= array[high]))
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		/* Probe the position with keeping uniform distribution in mind */
+		pos = low + (((double)(high - low) /
+					(array[high] - array[low])) * (value - array[low]));
 
-		printf("value checked array[%lu] = [%d]\n", pos, array[pos]);
+		/* Print the value being probed */
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
+		/* If the value is less than the probed position, adjust the high bound */
 		if (array[pos] < value)
 			low = pos + 1;
+
+		/* If the value is greater than the probed position, adjust the low bound */
 		else if (array[pos] > value)
-			high = pos -1;
+			high = pos - 1;
+
+		/* If the value is found, return the index */
 		else
 			return (pos);
 	}
 
+	/* If the value is equal to the low bound, return the low bound */
 	if (value == array[low])
 		return (low);
-	else
-	{
-		printf("value checked array[%lu] is out of range\n", pos);
-		return (-1);
-	}
+
+	/* If the value is not found, return -1 */
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
 }
